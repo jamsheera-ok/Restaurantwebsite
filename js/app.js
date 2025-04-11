614416f74923c3f99eda4fc59b9874338134e2a8
jamsheera-ok <jamsheeranas376@gmail.com>
2025-04-11 12:57:42 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+document.querySelector('.hamburger').addEventListener('click', () => {
+  document.querySelector('.nav-menu').classList.toggle('active');
+});
+var swiper = new Swiper (".food-slider",{
+    grabCursor:true,
+    loop :true,
+    CenteredSlides :true,
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        700: {
+          slidesPerView: 2,
+        },
+        1000: {
+          slidesPerView: 3,
+        },
+      },
+});
+
+
